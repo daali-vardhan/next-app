01ed88520381cf3b195633ad86de041bc4aae4c2
Daali Vardhan <daali_vardhan@epam.com>
2024-07-05 11:12:27 +0530

    initial commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  distDir: "dist",
+  basePath: "/next-app",
+  assetPrefix: "/next-app/",
+};
+
+export default nextConfig;
